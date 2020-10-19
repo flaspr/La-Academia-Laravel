c522a0540d681e79081ab5e3ef76412534825c72
flaspr <71883051+flaspr@users.noreply.github.com>
2020-10-19 22:06:20 +0200

    AHORA SI (?)

---
+++ resources/views/layouts/carousel.blade.php
@@ -0,0 +1,13 @@
+<div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
+    <div class="carousel-inner">
+        <div class="carousel-item active">
+            <img class="d-block w-100" src="{{ asset('storage/images/img1.jpg') }}" alt="First slide">
+        </div>
+        <div class="carousel-item">
+            <img class="d-block w-100" src="{{ asset('storage/images/img2.jpg') }}" alt="Second slide">
+        </div>
+        <div class="carousel-item">
+            <img class="d-block w-100" src="{{ asset('storage/images/img3.jpg') }}" alt="Third slide">
+        </div>
+    </div>
+</div>
