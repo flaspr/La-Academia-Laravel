1fd69ad10a5a1f4a9ad8edf6378bc9b40413519e
flaspr <71883051+flaspr@users.noreply.github.com>
2020-10-22 20:30:18 +0200

    Mejora de Navbar

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,27 +1,5 @@
 require("./bootstrap");
 require("./slick");
-
-$(".slider-header").slick({
-    lazyLoad: "ondemand",
-    dots: true,
-    infinite: true,
-    speed: 1000,
-    autoplaySpeed: 10000,
-    arrows: false,
-    fade: true,
-    cssEase: "linear",
-    autoplay: true,
-    centerMode: true,
-    slidesToShow: 1,
-    pauseOnFocus: false,
-    pauseOnHover: false,
-    responsive: [{
-        breakpoint: 480,
-        settings: {
-            arrows: false,
-            centerMode: true,
-            centerPadding: "40px",
-            slidesToShow: 1
-        }
-    }]
-});
+require("jquery");
+require("./shrink");
+require("./slider");
