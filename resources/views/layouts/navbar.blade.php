963876a7846ab870076d7be2d9765f49703781c5
mistermiad <miadmeyfour@hotmail.es>
2020-10-24 17:28:49 +0200

    boton contacto

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -27,7 +27,7 @@
                     </a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="{{ route('matricula') }}">
+                    <a data-toggle="modal" data-target="#ContactoModal">
                         Contacto
                     </a>
                 </li>
