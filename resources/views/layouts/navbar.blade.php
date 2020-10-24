845dfa9ba4582dcc67e96b719e2a55768c49b8ef
mistermiad <miadmeyfour@hotmail.es>
2020-10-24 17:21:12 +0200

    Prueba matricula 2 arreglo

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -27,7 +27,7 @@
                     </a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="{{ route('contacto') }}">
+                    <a class="nav-link" href="{{ route('matricula') }}">
                         Contacto
                     </a>
                 </li>
