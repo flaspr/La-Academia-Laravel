b7c8b257bcce302daa1aa279982d00a358aba727
flaspr <71883051+flaspr@users.noreply.github.com>
2020-10-31 00:24:36 +0100

    Cambio de Fuente y js de #logo-texto

--- public/js/app.js
+++ public/js/app.js
@@ -37963,19 +37963,25 @@ function scrollFunction() {
     if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
       document.getElementById("navbar").classList.remove("py-3");
       document.getElementById("logo").classList.remove("logo-grande");
+      document.getElementById("logo-texto").classList.remove("logo-texto-grande");
       document.getElementById("navbar").classList.add("py-1");
       document.getElementById("logo").classList.add("logo-pequeño");
+      document.getElementById("logo-texto").classList.add("logo-texto-pequeño");
     } else {
       document.getElementById("navbar").classList.remove("py-1");
       document.getElementById("logo").classList.remove("logo-pequeño");
+      document.getElementById("logo-texto").classList.remove("logo-texto-pequeño");
       document.getElementById("navbar").classList.add("py-3");
       document.getElementById("logo").classList.add("logo-grande");
+      document.getElementById("logo-texto").classList.add("logo-texto-grande");
     }
   } else {
     document.getElementById("navbar").classList.remove("py-3");
     document.getElementById("navbar").classList.add("py-1");
     document.getElementById("logo").classList.remove("logo-grande");
     document.getElementById("logo").classList.add("logo-pequeño");
+    document.getElementById("logo-texto").classList.remove("logo-texto-grande");
+    document.getElementById("logo-texto").classList.add("logo-texto-pequeño");
   }
 }
 
--- resources/js/shrink.js
+++ resources/js/shrink.js
@@ -12,18 +12,36 @@ function scrollFunction() {
         ) {
             document.getElementById("navbar").classList.remove("py-3");
             document.getElementById("logo").classList.remove("logo-grande");
+            document
+                .getElementById("logo-texto")
+                .classList.remove("logo-texto-grande");
             document.getElementById("navbar").classList.add("py-1");
             document.getElementById("logo").classList.add("logo-pequeño");
+            document
+                .getElementById("logo-texto")
+                .classList.add("logo-texto-pequeño");
         } else {
             document.getElementById("navbar").classList.remove("py-1");
             document.getElementById("logo").classList.remove("logo-pequeño");
+            document
+                .getElementById("logo-texto")
+                .classList.remove("logo-texto-pequeño");
             document.getElementById("navbar").classList.add("py-3");
             document.getElementById("logo").classList.add("logo-grande");
+            document
+                .getElementById("logo-texto")
+                .classList.add("logo-texto-grande");
         }
     } else {
         document.getElementById("navbar").classList.remove("py-3");
         document.getElementById("navbar").classList.add("py-1");
         document.getElementById("logo").classList.remove("logo-grande");
         document.getElementById("logo").classList.add("logo-pequeño");
+        document
+            .getElementById("logo-texto")
+            .classList.remove("logo-texto-grande");
+        document
+            .getElementById("logo-texto")
+            .classList.add("logo-texto-pequeño");
     }
 }
